b7e90c1699519f1c6fc44972df5f19f9f1d94c78
Jim Wright <jim@linx.net>
2017-09-08 14:23:34 +0100

    Tests for setApiKey

--- tests/PubgTest.php
+++ tests/PubgTest.php
@@ -3,6 +3,7 @@
 namespace JmWri\Pubg\Test;
 
 use JmWri\Pubg\Pubg;
+use JmWri\Pubg\PubgException;
 use Mockery as m;
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Psr7\Response;
@@ -16,6 +17,20 @@ use GuzzleHttp\Psr7\Response;
  */
 class PubgTest extends BaseTest
 {
+    public function testSetApiKey()
+    {
+        $pubg = new Pubg('old_api_key');
+        $this->assertEquals('old_api_key', $pubg->getApiKey());
+        $pubg->setApiKey('new_api_key');
+        $this->assertEquals('new_api_key', $pubg->getApiKey());
+    }
+
+    public function testSetApiKeyWithInvalid()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Pubg(null);
+    }
+
     public function testGetPlayerStats()
     {
         $body = Psr7\stream_for(file_get_contents(__DIR__ . '/data/get_player_stats.json'));
